051d028ac57cf95dd904d3594e138fcbbe82385b
Emanuel Zuluaga Jaramillo <ema.zulu.jar@gmail.com>
2026-08-10 09:10:31 -0500

    Creación de variables en java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+  
+        int a;
+        float b;
+        byte c;
+        double d;
+        int e;
+        float f;
+        byte g;
+        double h;
+        int j;
+        float k;
+        byte l;
+        double m;
+        int n;
+        float o;
+        byte p;
+        double q;
+        int r;
+        float s;
+        byte t;
+        double u;
+    }
+}
