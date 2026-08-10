9345d07243d667f55a665c2229743c7548a813d0
Emanuel Zuluaga Jaramillo <ema.zulu.jar@gmail.com>
2026-08-10 10:17:14 -0500

    Reasignacion de variables y valores

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
   
+        //Declaracion de variables
         int a;
         float b;
         byte c;
@@ -24,26 +25,46 @@ public class Main {
         byte t;
         double u;
 
-        a =1;
-        b = 2f;
-        c = 3;
-        d = 4.01;
-        e = 5;
-        f = 6f;
-        g = 7;
-        h = 8.01;
+        //Asignacion de valores a las variables
+        a = 1;//
+        b = 2f;//
+        c = 30;//
+        d = 4.01;//
+        e = 5;//
+        f = 6f;//
+        g = 78;//
+        h = 8.01;//
         j = 9;
         k = 10f;
-        l = 11;
+        l = 120;
         m = 12.01;
-        n = 13;
+        n = 13;//
         o = 14f;
-        p = 15;
+        p = -15;
         q = 16.01;
-        r = 17;
+        r = 17;//
         s = 18f;
-        t = 19;
+        t = 109;
         u = 20.01;
+
+        //Reasignacion de valores a las variables
+        a = e;
+        b = f;
+        c = g;
+        d = h;
+        n = r;
+
+        //Reasignacion de valores a otros datos hardcoded
+        j = 17;
+        k = 25.4f;
+        l = -27;
+        m = 49.2;
+        o = 50.3f;
+        p = -38;
+        q = 60.4;
+        s = 70.5f;
+        t = 80;
+        u = 90.6;
 
 
     }
