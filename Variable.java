ec66d69b9197b127ac7ebdd8a73e4e5c0474fc16
Emanuel Zuluaga Jaramillo <ema.zulu.jar@gmail.com>
2026-08-15 23:51:09 -0500

    Cambio de nombre a las variables primitivas

--- Variable.java
+++ Variable.java
@@ -5,67 +5,67 @@ public class Variable {
     public static void main(String[] args) {
   
         //Declaracion de variables
-        int a;
-        float b;
-        byte c;
-        double d;
-        int e;
-        float f;
-        byte g;
-        double h;
-        int j;
-        float k;
-        byte l;
-        double m;
-        int n;
-        float o;
-        byte p;
-        double q;
-        int r;
-        float s;
-        byte t;
-        double u;
+        int int1;
+        float float1;
+        byte byte1;
+        double double1;
+        int int2;
+        float float2;
+        byte byte2;
+        double double2;
+        int int3;
+        float float3;
+        byte byte3;
+        double double3;
+        int int4;
+        float float4;
+        byte byte4;
+        double double4;
+        int int5;
+        float float5;
+        byte byte5;
+        double double5;
 
         //Asignacion de valores a las variables
-        a = 1;//
-        b = 2f;//
-        c = 30;//
-        d = 4.01;//
-        e = 5;//
-        f = 6f;//
-        g = 78;//
-        h = 8.01;//
-        j = 9;
-        k = 10f;
-        l = 120;
-        m = 12.01;
-        n = 13;//
-        o = 14f;
-        p = -15;
-        q = 16.01;
-        r = 17;//
-        s = 18f;
-        t = 109;
-        u = 20.01;
+        int1 = 1;//
+        float1 = 2f;//
+        byte1 = 30;//
+        double1 = 4.01;//
+        int2 = 5;//
+        float2 = 6f;//
+        byte2 = 78;//
+        double2 = 8.01;//
+        int3 = 9;
+        float3 = 10f;
+        byte3 = 120;
+        double3 = 12.01;
+        int4 = 13;//
+        float4 = 14f;
+        byte4 = -15;
+        double4 = 16.01;
+        int5 = 17;//
+        float5 = 18f;
+        byte5 = 109;
+        double5 = 20.01;
 
         //Reasignacion de valores a las variables
-        a = e;
-        b = f;
-        c = g;
-        d = h;
-        n = r;
+        int1 = int2;
+        float1 = float2;
+        byte1 = byte2;
+        double1 = double2;
+        int4 = int5;
 
         //Reasignacion de valores a otros datos hardcoded
-        j = 17;
-        k = 25.4f;
-        l = -27;
-        m = 49.2;
-        o = 50.3f;
-        p = -38;
-        q = 60.4;
-        s = 70.5f;
-        t = 80;
-        u = 90.6;
+        int3 = 17;
+        float3 = 25.4f;
+        byte3 = -27;
+        double3 = 49.2;
+        float4 = 50.3f;
+        byte4 = -38;
+        double4 = 60.4;
+        float5 = 70.5f;
+        byte5 = 80;
+        double5 = 90.6;
  
         //Creación de variable char
 
@@ -92,7 +92,7 @@ public class Variable {
                     break;
             
                     case 2:
-
+                    //Ecuacion 2
                     break;
 
                     case 3:
