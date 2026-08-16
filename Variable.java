8b0c8fd9b3621f68ee7319d21ed04b1aa65899b0
Emanuel Zuluaga Jaramillo <ema.zulu.jar@gmail.com>
2026-08-15 23:40:53 -0500

    Se agrega el programa que permita solucionar la primera ecuación

--- Variable.java
+++ Variable.java
@@ -1,3 +1,6 @@
+import javax.swing.JOptionPane;
+
+
 public class Variable {
     public static void main(String[] args) {
   
@@ -63,9 +66,44 @@ public class Variable {
         s = 70.5f;
         t = 80;
         u = 90.6;
+ 
+        //Creación de variable char
 
-        int suma = a + e + n + j + l + p + t;
-        System.out.println("La suma de las variables enteras es: " + suma);
 
+        
+
+        //Programa 
+        int opcion;
+        Double x; 
+        Double y;
+        Double z; 
+
+
+        JOptionPane.showMessageDialog(null, "Bienvenido a nuestra calculadora de ecuaciones");
+        
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1) Solución de ecuación 1 \n 2) Solución de ecuación 2 \n 3) Salida"));
+                switch (opcion) {
+                    case 1:
+                    x = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el valor de x"));
+                    z  = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el valor de z"));
+                    y = (3*x)/((1+(3*x)/(3*(z*z)+2))/(1/((1/(1+z))+(3*(x*x))+(2*z)+3)));
+                    JOptionPane.showMessageDialog(null, "Este es el valor de y: " + y);
+                    break;
+            
+                    case 2:
+
+                    break;
+
+                    case 3:
+                    JOptionPane.showMessageDialog(null, "Gracias por usar nuestra calculadora de ecuaciones");
+
+                 default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida, digíte una opción válida");
+
+                    break;
+                }
+        }
+        while (opcion != 1 && opcion != 2 && opcion != 3);
     }
 }
