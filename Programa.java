3788d0f0dc90ddea08e9c669a2f4ac0c08ce2b54
suri <surisaddai03@gmail.com>
2026-08-18 00:51:48 -0500

    Arreglos y ajustes del programa con los ejercicios

--- Programa.java
+++ Programa.java
@@ -1,119 +1,80 @@
-import javax.swing.JOptionPane;
+// Surisaddai Pulgarin Torres
+// Emanuel Zuluaga 
+import java.util.Scanner; // Se importó el Scanner 
 
 public class Programa {
     public static void main(String[] args) {
-        
-        // Programa
+
+        Scanner inicio = new Scanner(System.in);
+        // Aun no se inicializan las variables 
+        // Pero si se declaran las variables 
         int opcion = 0;
-        int pregunta = 0;
-        Double x = 0.0;
-        Double y = 0.0;
-        Double z = 0.0;
-        boolean entradaValida = false;
+        double y; 
+        double x; 
+        double z; 
 
-        JOptionPane.showMessageDialog(null, "Bienvenido a nuestra calculadora de ecuaciones");
+        while (opcion != 3) {
+            //Inicio del programa, se usa while para 
+            //que el programa no pare a menos de que le des a la 
+            //opción de salida
+            System.out.println("_______________________________");
+            System.out.println("\n---- BIENVENIDO AL PROGRAMA ----" );
+            System.out.println(" Por favor ingrese una opción: ");
+            System.out.println("_______________________________");
+            System.out.println("\n[1] Ejercicio 1");
+            System.out.println("[2] Ejercicio 2");
+            System.out.println("[3] Salir");
 
-        do {
-            do {
-                try {
-                    opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
-                            "1) Solución de ecuación 1 \n 2) Solución de ecuación 2 \n 3) Salida"));
-                    entradaValida = true;
-                } catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, "Debe ingresar una opción válida. \n Tipo de error: " + e);
-                }
-            } while (!entradaValida);
-            entradaValida = false;
+            opcion = inicio.nextInt();
 
-            switch (opcion) {
-                case 1:
-                    do {
-                        try {
-                            x = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el valor de x"));
-                            entradaValida = true;
-                        } catch (Exception e) {
-                            JOptionPane.showMessageDialog(null,
-                                    "Debe ingresar una opción válida. \n Tipo de error: " + e);
-                        }
-                    } while (!entradaValida);
-                    entradaValida = false;
-                    do {
-                        try {
-                            z = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el valor de z"));
-                            entradaValida = true;
-                        } catch (Exception e) {
-                            JOptionPane.showMessageDialog(null,
-                                    "Debe ingresar una opción válida. \n Tipo de error: " + e);
-                        }
-                    } while (!entradaValida);
-                    entradaValida = false;
-                    do {
-                        try {
-                            y = (3 * x) / ((1 + (3 * x) / (3 * (z * z) + 2))
-                                    / (1 / ((1 / (1 + z)) + (3 * (x * x)) + (2 * z) + 3)));
-                            entradaValida = true;
-                        } catch (Exception e) {
-                            JOptionPane.showMessageDialog(null,
-                                    "Debe ingresar una opción válida. \n Tipo de error: " + e);
-                        }
-                    } while (!entradaValida);
-                    entradaValida = false;
-                    JOptionPane.showMessageDialog(null, "Este es el valor de y: " + y);
+            if (opcion == 1) {
+                //Primer caso, no hay problema si alguna de las dos 
+                //variables es cero 
+                System.out.print("Elige que número quieres que tenga la variable [x]: ");
+                x = inicio.nextDouble();
+                System.out.print("Elige que número quieres que tenga la variable [z]: ");
+                z = inicio.nextDouble();
 
-                    do {
-                        do {
-                            try {
-                                pregunta = Integer.parseInt(JOptionPane.showInputDialog(null,
-                                        "¿Desea volver hacer una ecuación? \n 1) SI \n 2) NO"));
-                                entradaValida = true;
-                            } catch (Exception e) {
-                                JOptionPane.showMessageDialog(null,
-                                        "Debe ingresar una opción válida. \n Tipo de error: " + e);
-                            }
-                        } while (!entradaValida);
-                        entradaValida = false;
+                y = ((3*x/(1 + (3*x/(((3*(z*z)) + 2)/(1/((1/(1 + z))+(3*(x*x))+(2*z)+3)))))));
 
-                        if (pregunta != 1 && pregunta != 2) {
-                            JOptionPane.showMessageDialog(null, "Opción no válida. Ingrese 1 para SI o 2 para NO.");
-                        }
-                    } while (pregunta != 1 && pregunta != 2);
-
-                    if (pregunta == 1) {
-                        opcion = 0;
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Gracias por usar nuestra calculadora de ecuaciones");
-                        opcion = 3;
-                    }
-                    break;
-
-                case 2:
-                    // Ecuacion 2 poner
-                    break;
-
-                case 3:
-                    JOptionPane.showMessageDialog(null, "Gracias por usar nuestra calculadora de ecuaciones");
-                    break;
-
-                default:
-                    JOptionPane.showMessageDialog(null, "Opción no válida, digíte una opción válida");
-                    break;
+                System.out.println("El resultado de la operación es igual a :" + y);
             }
-        } while (opcion != 3);
 
+            else if (opcion == 2) {
+                //Segundo caso, [z] no puede ser igual a cero
+                System.out.print("Elige que número quieres que tenga la variable [x]: ");
+                x = inicio.nextDouble();
+                System.out.print("Elige que número quieres que tenga la variable [z]: ");
+                z = inicio.nextDouble();
 
+                //Se usa While para que el usuario ingrese un 
+                //número distinto a 0 en [z] 
+                //Se va a repetir siempre y cuando [z] sea distinto a 0
+                while (z == 0){
 
+                System.out.println("El valor de [z] debe ser distinto de 0 ");
+                System.out.print("Intenta de nuevo. Ingrese el valor para [z]: ");
+                z = inicio.nextDouble();   
+                
+                }
+                 
+                y = ((((x*x) + (3*z) + 2)/(2+(1/(1+(2*z)))))+(2/((1+(3*(x*x*x)+(3*z)+2))/1/(1/((1+(3*x))+((3*x)+1/((2*z)+3/(1 + (5/z)))))))));
+                System.out.println("El resultado es : "+ y);                
+            }
 
+            else if (opcion == 3){
+                //Tercer caso, solo sale del programa 
+                System.out.print("Saliendo del programa...¡Gracias por su visita!");
+            }
 
-
-
-
-
-
-
-
-
-
-
+            else {
+                //Cuarto caso, se va a repetir el programa hasta que 
+                //el usuario elija una opción válida 
+                System.out.println("Opción no válida. Por favor, intenta de nuevo.");
+            }
+              
+        }
+        
 
 
     }
